d7616b899650dda61b9af5b75f07d82abea1d86f
inniy2 <inniy2@gmail.com>
2018-11-09 16:49:32 +0900

    Init

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,5 +1,62 @@
 #
-# Cookbook:: mysql-5.7.11
+# Cookbook:: chef-mysql
 # Recipe:: default
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
+
+# wget https://dev.mysql.com/get/mysql57-community-release-el7-11.noarch.rpm
+remote_file '/usr/local/src/mysql57-community-release-el7-11.noarch.rpm' do
+    source 'https://dev.mysql.com/get/mysql57-community-release-el7-11.noarch.rpm'
+    action :create
+end
+
+# sudo yum localinstall mysql57-community-release-el7-11.noarch.rpm
+yum_package 'mysql57-community-release-el7-11.noarch.rpm' do
+    source '/usr/local/src/mysql57-community-release-el7-11.noarch.rpm'
+    action :install
+end
+
+# sudo yum install mysql-community-server
+yum_package 'mysql-community-server' do
+    action :install
+end
+
+# sudo service mysqld start
+service 'mysqld' do
+    action [ :enable, :start ]
+end
+
+# Create /usr/local/src/get_init_password
+template '/usr/local/src/get_init_password' do
+    source 'get_init_password.erb'
+    mode '0755'
+end
+
+# Run /usr/local/src/get_init_password
+execute 'run_the_get_script' do
+    command '/usr/local/src/get_init_password'
+    action :nothing
+    subscribes :run, 'template[/usr/local/src/get_init_password]', :immediate
+end
+
+# Run  echo "ALTER USER 'root'@'localhost' IDENTIFIED BY 'Whatever_you_said'; FLUSH PRIVILEGES " | mysql -uroot -p$pw
+# mysql -uroot -p`cat /var/log/init_pw.log` -e 'alter user "root"@"localhost" identified by "Whatever_you_said"'
+execute 'run_alter_user' do
+    command 'echo " alter user root@localhost identified by "\'"Whatever_you_said0@"\' | mysql --connect-expired-password -uroot -p`cat /var/log/init_pw.log`'
+    action :nothing
+    subscribes :run, 'execute[run_the_get_script]', :delayed
+end
+
+# TO-DO: Following scripts are NOT YET tested! Run it first and see what happens!!
+
+# Create /usr/local/src/init_user
+template '/usr/local/src/init_user' do
+    source 'init_user.erb'
+end
+
+# Run  cat /usr/local/src/init_user | mysql -uroot -p"Whatever_you_said0@"
+execute 'run_init_user' do
+    command 'cat /usr/local/src/init_user | mysql -uroot -pWhatever_you_said0@'
+    action :nothing
+    subscribes :run, 'template[/usr/local/src/init_user]', :delayed
+end
--- test/smoke/default/default_test.rb
+++ test/smoke/default/default_test.rb
@@ -1,6 +1,6 @@
 # # encoding: utf-8
 
-# Inspec test for recipe mysql-5.7.11::default
+# Inspec test for recipe chef-mysql::default
 
 # The Inspec reference, with examples and extensive documentation, can be
 # found at http://inspec.io/docs/reference/resources/
@@ -15,4 +15,105 @@ end
 # This is an example test, replace it with your own test.
 describe port(80), :skip do
   it { should_not be_listening }
+end
+
+
+# My end goal
+# describe command('mysql -uroot -pWhatever_you_said0@ -e \'select \"select version()\"\'') do
+    # its('stdout') { should match ('/5.7/') }
+# end
+sql = mysql_session('root','Whatever_you_said0@','localhost','3306')
+describe sql.query('select version() as version') do
+    its('stdout') { should match(/5.7/) }
+end
+
+# wget https://dev.mysql.com/get/mysql57-community-release-el7-11.noarch.rpm
+describe file('/usr/local/src/mysql57-community-release-el7-11.noarch.rpm') do
+    it { should exist }
+end
+
+# sudo yum localinstall mysql57-community-release-el7-11.noarch.rpm
+describe yum do
+    its('mysql57-community/x86_64') { should exist }
+end
+
+describe yum.repo('mysql57-community/x86_64') do
+    it { should exist }
+    it { should be_enabled }
+    its('baseurl') { should include '5.7' }
+end
+
+# TO DO: Verify it
+# yum repolist enabled | grep "mysql.*-community.*"
+
+# SKIP: 
+# sudo yum-config-manager --disable mysql56-community
+
+# SKIP: 
+# sudo yum-config-manager --enable mysql57-community
+
+# SKIP: 
+# vi /etc/yum.repos.d/mysql-community.repo
+
+# sudo yum install mysql-community-server
+describe package('mysql-community-server') do
+    it { should be_installed }
+end
+
+# sudo service mysqld start
+describe service ('mysqld') do
+    it { should be_enabled }
+    it { should be_running }
+end
+
+
+# sudo grep 'temporary password' /var/log/mysqld.log
+describe file('/var/log/mysqld.log') do
+    it { should exist }
+    its('content') { should match /root@localhost:/ }
+end
+
+describe port(3306) do
+  it { should be_listening }
+  its('processes') {should include 'mysqld'}
+end
+
+# Create /usr/local/src/get_init_password
+#
+# #!/bin/bash
+# T_PW=""
+# string=`cat /var/log/mysqld.log | grep "localhost:"
+# for word in $string
+# do
+#     T_WP=$word
+# done
+# echo $T_PW > /var/log/init_pw.log
+#
+# pw=`cat /var/log/init_pw.log`
+# 
+#
+describe file('/usr/local/src/get_init_password') do
+    it { should exist }
+    its('mode') { should cmp '00755' }
+end
+
+# Run /usr/local/src/get_init_password
+describe file('/var/log/init_pw.log') do
+    it { should exist }
+    its('content') { should match(/[a-z]/) }
+end
+
+# echo "ALTER USER 'root'@'localhost' IDENTIFIED BY 'Whatever_you_said'; FLUSH PRIVILEGES " | mysql -uroot -p$pw
+
+
+# Create /usr/local/src/init_user
+describe file('/usr/local/src/init_user') do
+    it { should exist }
+    its ('content') { should match(/[privileges]/) }
+end
+
+# Run /usr/local/src/init_user
+sql = mysql_session('root','Whatever_you_said0@','localhost','3306')
+describe sql.query('show grants for \'root\'@\'%\' ') do
+    its('stdout') { should match(/PRIVILEGES/) }
 end
